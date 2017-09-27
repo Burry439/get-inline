4caabe2e42877fa42a2fa7b9ef191510a2c5bff5
burry <burry439@gmail.com>
2017-09-27 11:18:48 +0300

    socket in progress

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var app = express();
+var server = require('http').createServer(app)
+var io = require('socket.io')(server);
 var bodyParser = require('body-parser');
 var Student = require('./models/students.js')
 var Teacher = require('./models/teachers.js')
@@ -8,13 +11,18 @@ mongoose.connect('mongodb://localhost/get-inlineDB', function(){
     console.log('DB is on!!!! XD ;)')
 })
 
-var app = express();
+
 app.use(express.static('public'));
 app.use(express.static('assets'));
 app.use(express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.listen(8000, function(){
+io.on('connection', function(client){
+    console.log('working')
+    client.emit('messages', {hello : 'hello'});
+})
+
+server.listen(8000, function(){
     console.log('listening on port 8000!!!XD')
 })
